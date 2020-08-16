9cd5a5accbfa47f44312253f6c1e78c70fc407a0
asmsufian <asmsufian.23@gmail.com>
2020-08-15 23:47:29 -0700

    newjs

--- new.js
+++ new.js
@@ -1,3 +1,6 @@
+// class theke new use kore object banano jay shetai dekhano hoise
+
+
 class Person{
     constructor(firstName, lastName, salary) {
         this.firstName = firstName;
@@ -5,3 +8,17 @@ class Person{
         this.salary = salary;
     }
 }
+
+const heroPerson = new Person('balam', 'sazid', 2000);
+console.log(heroPerson);
+const friendlyPerson = new Person('salam', 'sazid', 15000);
+console.log(friendlyPerson);
+
+function Person1(firstName, lastName, salary){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.salary = salary;
+}
+
+const oldPerson = new Person1('grand', 'papa', 1200);
+ console.log(oldPerson);
